d5bfb85d2f7dceb80331524b07c629cb86bc1746
Ishaan96 <ishaan.sengupta2@gmail.com>
2019-07-31 18:32:04 +0530

    Mainscript updated

--- blackjack_mainscript.py
+++ blackjack_mainscript.py
@@ -1,5 +1,16 @@
-from deck import Deck
+from deck import x
+from hand import Hand
+
 
 suits = ('Hearts','Diamonds','Clubs','Spades')
 ranks = ('Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten','Jack','Queen','King','Ace')
 values = {'Two':2,'Three':3,'Four':4,'Five':5,'Six':6,'Seven':7,'Eight':8,'Nine':9,'Ten':10,'Jack':10,'Queen':10,'King':10,'Ace':[1,11]}
+
+x.shuffle()
+player_hand = Hand()
+dealer_hand = Hand()
+player_hand.add_card(x.deal())
+player_hand.add_card(x.deal())
+dealer_hand.add_card(x.deal())
+dealer_hand.add_card(x.deal())
+
